b2abc7f3dc6ac01d71e899f4816b26f9e79cfd39
Ana TA <gacelita@gmail.com>
2018-06-03 22:39:59 +0000

    lightbox

--- _site/assets/scripts.js
+++ _site/assets/scripts.js
@@ -3,5 +3,20 @@ $( document ).ready(function() {
     function() {
       $( '#menu' ).toggle(400);
     }
-  )
+  );
+
+    // Open Lightbox
+    $('.lightbox > a').on('click', function(e) {
+      e.preventDefault();
+      var image = $(this).attr('href');
+      $('html').addClass('no-scroll');
+      $('body').append('<div class="lightbox-opened"><img src="' + image + '"></div>');
+    });
+
+    // Close Lightbox
+      $('body').on('click', '.lightbox-opened', function() {
+      $('html').removeClass('no-scroll');
+      $('.lightbox-opened').remove();
+    });
+    
 });
